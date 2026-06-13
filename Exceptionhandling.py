7fa9371a8e83be3b6a1e2eb055fb5845cc46472e
Vansh Viraj Bisht <vanshbisht74@gmail.com>
2026-06-14 02:32:01 +0530

    Add files via upload

---
+++ Exceptionhandling.py
@@ -0,0 +1,29 @@
+# IndexError
+# The IndexError is thrown when trying to access an item at an invalid index.
+
+# ModuleNotFoundError
+# The ModuleNotFoundError is thrown when a module could not be found.
+
+# KeyError
+# The KeyError is thrown when a key is not found.
+
+# TypeError
+# The TypeError is thrown when an operation or function is applied to an object of an inappropriate type.
+
+#name error 
+# Name error is thrown when an object is not found (like not declared and printing it)
+
+
+
+
+#THESE ERROR MESSAGES IN CONSOLE OF ANY TYPE AND KNOW AS ""STACK TRACE"".
+# AttributeError
+
+# Traceback (most recent call last)
+
+# <ipython-input-80-dd5a29625ddc> in <module>
+#       1 # AttributeError
+#       2 L = [1,2,3]
+# ----> 3 L.upper()
+
+# AttributeError: 'list' object has no attribute 'upper'
