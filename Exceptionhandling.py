1e49545ddfed6cb7387e681b5fcea3e865f80d4a
Vansh Viraj Bisht <vanshbisht74@gmail.com>
2026-06-15 01:10:31 +0530

    Add files via upload

--- Exceptionhandling.py
+++ Exceptionhandling.py
@@ -27,3 +27,147 @@
 # ----> 3 L.upper()
 
 # AttributeError: 'list' object has no attribute 'upper'
+
+
+
+# Catching specipic exeption
+try:
+    m=5
+    f=open("select.txt","r")
+    
+
+#ERRORS ARE CATCHED HERE
+except FileNotFoundError:
+    print("File Not Found")
+except NameError:
+    print("Varible not found")
+except Exception as e:
+    print(e)
+
+# We Write that code inside the else block and that needs to execute when their is no
+#Error in the try block , when the exept block is skiped.
+else:
+    print(m)
+    print(f.read())
+
+
+finally:
+    print("THIS WILL PRINT ")
+
+
+# ----------------------------------------------------------------------------------
+#RAISE
+class Bank:
+    def __init__(self,balance):
+        self.balance=balance
+
+    def withdraw(self,amount):
+        if amount <=0:
+            raise Exception("Amount can't be negative or zero ")
+        if amount>self.balance:
+            raise("Insufficient Funds")
+        
+        else:
+            self.balance-=amount
+
+obj=Bank(10000)
+
+try:
+    obj.withdraw(1000)
+
+except Exception as e:
+    print(e)
+
+else:
+    print("Remaining Funds",obj.balance)
+
+finally:
+    print("Thank you for using")
+
+
+
+#----------------------------------------------------------------------------------------
+#MAKING OWN CUSTOM CLASSES FOR EXCEPTION HANDLING 
+class Myexception(Exception):
+  def __init__(self,message):
+    print(message)
+
+
+class Bank:
+    def __init__(self,balance):
+        self.balance=balance
+
+    def withdraw(self,amount):
+        if amount <=0:
+            raise Myexception("Amount can't be negative or zero ")
+        if amount>self.balance:
+            raise Myexception("Insufficient Funds")
+        
+        else:
+            self.balance-=amount
+
+obj=Bank(10000)
+
+try:
+    obj.withdraw(1000000)
+
+except Myexception as e:
+  pass
+
+else:
+    print("Remaining Funds",obj.balance)
+
+finally:
+    print("Thank you for using our system")
+
+
+
+#--------------------------------------------------------------------------------------------
+
+#CUSTOM CLASSES
+
+
+
+class SecurityError(Exception):
+  def __init__(self,message):
+      print(message)
+  def logout(self):
+    print("Logout done")
+
+class Google:
+  def __init__(self,name,email,password,device):
+      self.name=name
+      self.email=email
+      self.password=password
+      self.device=device
+  
+
+  def login(self,email,password,device):
+        
+        if device!=self.device:
+          raise SecurityError("LOGIN FROM A NEW OR UNREGISTERED DEVICE FOR SECURITY REASONS LOGGING OUT FROM ALL DEVICES.......")
+        if email==self.email and password==self.password:
+          print("WELCOME.....")
+
+        else:
+          print("WRONG EMAIL OR PASSWORD PLEASE TRY AGAIN")
+
+
+obj=Google("Vansh","vanshbisht74@gmail.com","1234","android")
+
+try:
+  obj.login("vanshbisht74@gmail.com","1234","IOS")
+
+except SecurityError as e:
+  e.logout()
+
+else:
+  print("--->",obj.name)
+
+finally:
+  print("DATABASE CONNECTION DISCONNECTED ")
+
+
+
+  
+#-------------------------------------------------------------------------------------
