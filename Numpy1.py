afcb58142fb1dc7038ba428a9f0a373b147b770a
Vansh Viraj Bisht <vanshbisht74@gmail.com>
2026-06-19 22:22:09 +0530

    Add files via upload

--- Numpy1.py
+++ Numpy1.py
@@ -80,17 +80,280 @@ a1.shape
 print(a2.size)
 
 
-#item size --->>>> tells how much each item acquire the shape
-a2.itemsize
+# #item size --->>>> tells how much each item acquire the shape
+# a2.itemsize
 
 
-#dtype----.tells the data type 
-a2.dtype
-a1.dtype
+# #dtype----.tells the data type 
+# a2.dtype
+# a1.dtype
 
 
 
 
-#CHANGING DATATYPES
+# #CHANGING DATATYPES
 
-a2.astype(np.int32) #reducing the space taken from int64------> to int 32..
+# a2.astype(np.int32) #reducing the space taken from int64------> to int 32..
+
+
+
+
+#SCALAR OPERATIONS
+import numpy as np
+a1=np.arange(1,11).reshape(5,2)
+a2=np.arange(12,24).reshape(3,4)
+
+#arthimatic op
+print(a1)
+print(a1*2)
+print(a1-2)
+print(a1+4)
+
+#relational operators
+print(a2>5)
+print(a2==6)
+
+#VECTOR
+#SAME  SHAPE OF TWO ARRAY THEN 
+# a1+a2 can be performed and all things also
+
+
+#Array functions
+import numpy as np
+n1=np.random.random((3,3))
+n1=np.round(n1*100)
+print(n1)
+
+np.max(n1)
+np.min(n1)
+np.sum(n1)
+np.prod(n1)
+
+
+
+#row max
+np.max(n1,axis=1) #riw wise
+
+
+#column max
+np.max(n1,axis=0)  #column wise
+
+
+
+#MEAN
+np.mean(n1)
+
+#median
+np.median(n1)
+
+
+#variance
+np.var(n1)
+
+
+#trignometric funtions 
+np.sin(n1)
+
+
+
+np.cos(n1)
+
+
+
+
+# DOT PRODUCT 
+# Criteria for Dot Product
+# 1. Two 1-D Arrays (Vectors)
+
+# Both vectors must have the same number of elements.
+
+import numpy as np
+
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+print(np.dot(a, b))
+
+# Output:
+
+32
+
+# Calculation:
+
+# (1×4) + (2×5) + (3×6) = 32
+# 2. Two 2-D Arrays (Matrices)
+
+# Number of columns of the first matrix = Number of rows of the second matrix
+
+# If A is (m × n) and B is (n × p), then:
+
+# A · B = (m × p)
+
+# Example:
+
+# A = np.array([[1, 2],
+#               [3, 4]])
+
+# B = np.array([[5, 6],
+#               [7, 8]])
+
+# print(np.dot(A, B))
+
+# Output:
+
+# [[19 22]
+#  [43 50]]
+import numpy as np
+a1=np.arange(1,13).reshape(3,4)
+a2=np.arange(12,24).reshape(4,3)
+print(a1)
+print(a2)
+
+print(np.dot(a1,a2))4
+
+np.log(a1)
+
+
+
+# #ROUND OFF TO THE NEAREST INTERGER
+# np.round(np.random.random((3,3))*100)
+
+# #FLOOR  -> rounds to previous interger
+# np.floor(np.random.random((3,3))*100)
+
+# np.ceil(np.random.random((3,3))*100)
+# #ceil-> rounds to suceeeding interger
+
+
+
+
+
+
+
+
+
+
+
+#-------------------------------------------------------------------------------------
+
+#INDEXING AND SLICING 
+
+
+
+
+
+import numpy as np
+
+a1=np.arange(1,13)
+a2=np.arange(12,24).reshape(4,3)
+a3=np.arange(8).reshape(2,2,2)
+
+
+# print(a1)
+
+
+# print(a1[3])
+
+# #FOR 2D
+# print(a2)
+# # print([row][column])
+# print(a2[2][2]) #print 20
+# print(a2[1][2]) #print 17
+
+
+# #FOR 3D 
+# print(a3)
+
+# # [[[0 1]
+# #   [2 3]]
+
+# #  [[4 5]
+# #   [6 7]]]
+
+# # print([konse no. matrix me hai][row],[column])
+# print(a3[1][0][1]) #prints 5
+# print(a3[0][0][1]) #prints 1
+
+
+# #Slicing
+# print(a1[1:5:2])
+
+
+#2D
+# print(a2)
+# # print(a2[:,1])
+
+
+# print()
+# print(a2[2:,0:2])
+
+# a4=np.arange(12).reshape(3,4)
+# print(a4)
+# print(a4[1,::3])
+
+# print(a4[0:2,1:])
+
+
+
+
+
+# a5=np.arange(27).reshape(3,3,3)
+# print(a5)
+# print()
+# print(a5[1][:][:])
+# print(a5[0][1])
+
+# print()
+# print(a5[1,:,1])
+
+#ITERATION
+for i in a1:
+    print(a1)
+
+#2D   prints 2d arrays 
+for i in a2:
+    print(a2)
+
+#3D
+for i in a3:
+    print(a3) #prints the 3-2d arrays
+
+#To print the items of array 1 by 1 we use the nditer func.
+
+for i in np.nditer(a3):
+    print(i)
+
+
+
+# #RESHAPING 
+# #1.TRANSPOSE
+# print(np.tranpose(a2)) 
+# #or 
+# a2.T  
+
+
+# #RAVEL --> CONVERTS ANY DIMENSIONAL ARRAY TO THE 1 D ARRAY.
+# print(a3.ravel)
+
+#----------------------------------------------------------------------------------------
+#STACKING
+#shape should be same 
+#HORIZONTAL STACKING
+import numpy as np
+a1=np.arange(12).reshape(3,4)
+a2=np.arange(12,24).reshape(3,4)
+
+
+#hstacking 
+
+print(np.hstack((a1,a2,a1)))
+
+#v stackin
+
+print(np.vstack((a1,a2,a1)))
+#------------------------------------------------------------------------------------------
+#Horizontal spliting
+
+np.hsplit(a2,2)  #splited in 2 equal halfs   dono dekhne me ulte hai okay.
+
+np.vsplit(a2,2)  # splited in two halves . v
